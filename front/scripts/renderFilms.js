c2dfaf388a6465a251998a34c64cc9f7e832c1cf
leoleguizamonpy <leoleguizamonpy@gmail.com>
2024-03-20 15:30:00 -0300

    Se añade el bundle.js y se realiza la modularizacion

---
+++ front/scripts/renderFilms.js
@@ -0,0 +1,21 @@
+const filmsSection = document.getElementById("films")
+
+function renderFilms(movie) {
+    const movieElement = document.createElement("article")
+    const containerMovie = document.createElement("div")
+    movieElement.classList.add("movie")
+    containerMovie.classList.add("divMovie")
+
+    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`
+
+    containerMovie.innerHTML = `<h5>${movie.title} (${movie.year}
+        </h5><p><strong>Director: </strong>${movie.director}</p>
+        <p><strong>Duración: </strong>${movie.duration}</p>
+        <p><strong>Género: </strong> ${movie.genre.join(", ")}</p>
+        <p><strong>Rate: </strong>${movie.rate}</p>
+        `
+    filmsSection.appendChild(movieElement)
+    movieElement.appendChild(containerMovie)
+}
+
+module.exports = renderFilms;
